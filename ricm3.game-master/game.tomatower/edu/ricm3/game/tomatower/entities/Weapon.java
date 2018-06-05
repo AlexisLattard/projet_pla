08e01d4a50a3788480c8683d7891c91b8bbc49b4
Romain <romain.guillot@etu.univ-grenoble-alpes.fr>
2018-06-05 11:49:30 +0200

    Hit du weapon

--- ricm3.game-master/game.tomatower/edu/ricm3/game/tomatower/entities/Weapon.java
+++ ricm3.game-master/game.tomatower/edu/ricm3/game/tomatower/entities/Weapon.java
@@ -1,5 +1,6 @@
 package edu.ricm3.game.tomatower.entities;
 
+
 import edu.ricm3.game.tomatower.entities.enums.Direction;
 import edu.ricm3.game.tomatower.map.Cell;
 import edu.ricm3.game.tomatower.mvc.Model;
@@ -35,6 +36,24 @@ public class Weapon {
 		/*
 		 * Cell cell_hit = ; cell_hit.damage(this.power);
 		 */
+		Cell shot = getCellToShot(origin_cell, origin_direction);
+		if(shot != null)
+			shot.damage(this.power);
+		
+	}
+	
+	public Cell getCellToShot(Cell origin_cell, Direction origin_direction) {
+		Direction dir = this.direction;
+		if((origin_direction == Direction.UP && dir == Direction.UP) || (origin_direction == Direction.RIGHT && dir == Direction.LEFT) || (origin_direction == Direction.LEFT && dir == Direction.RIGHT) || (origin_direction == Direction.DOWN && dir == Direction.DOWN)) {
+			return this.model.getCurrentMap().getCell(origin_cell.getPosition()[0], origin_cell.getPosition()[1]-1);
+		} else if((origin_direction == Direction.UP && dir == Direction.RIGHT) || (origin_direction == Direction.RIGHT && dir == Direction.UP) || (origin_direction == Direction.LEFT && dir == Direction.DOWN) || (origin_direction == Direction.DOWN && dir == Direction.LEFT)) {
+			return this.model.getCurrentMap().getCell(origin_cell.getPosition()[0] + 1, origin_cell.getPosition()[1]);
+		} else if((origin_direction == Direction.UP && dir == Direction.LEFT) || (origin_direction == Direction.RIGHT && dir == Direction.DOWN) || (origin_direction == Direction.LEFT && dir == Direction.UP) || (origin_direction == Direction.DOWN && dir == Direction.RIGHT)) {
+			return this.model.getCurrentMap().getCell(origin_cell.getPosition()[0] - 1, origin_cell.getPosition()[1]);
+		} else if((origin_direction == Direction.UP && dir == Direction.DOWN) || (origin_direction == Direction.RIGHT && dir == Direction.RIGHT) || (origin_direction == Direction.LEFT && dir == Direction.LEFT) || (origin_direction == Direction.DOWN && dir == Direction.DOWN)) {
+			return this.model.getCurrentMap().getCell(origin_cell.getPosition()[0], origin_cell.getPosition()[1] + 1);
+		}
+		return null; 
 	}
 
 	public void upgrade() {
